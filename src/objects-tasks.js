010408e1b231dfe4ad6829ac05b03401ddc79e9f
Aleks Gomeniuk <agsfinks@gmail.com>
2025-03-05 23:46:16 +0000

    feat: JSON stringify for getJSON

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -188,6 +188,12 @@ function Rectangle(width, height) {
       return width * height;
     },
   };
+  //   this.width = width;
+  //   this.height = height;
+
+  //   this.getArea = function() {
+  //     return this.width * this.height;
+  //   };
 }
 
 /**
@@ -200,8 +206,8 @@ function Rectangle(width, height) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { height: 10, width: 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 /**
